8c58b62dd21102cef69f3a1e851835ccf4575081
Kev <kdevcse@protonmail.com>
2023-03-27 02:36:48 -0500

    Cleanup Repository Code / Remove SignalR

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { HubConnectionBuilder } from "@microsoft/signalr";
-
-const connection = new HubConnectionBuilder()
-  .withUrl("http://localhost:5000/signalr")
-  .build();
 
 createApp(App).mount("#app");
