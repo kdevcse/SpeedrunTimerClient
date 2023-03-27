64aab2bc506d8d5879c77d0d1c334311d4fc33e0
Kev <kdevcse@protonmail.com>
2023-03-27 02:22:01 -0500

    Add assets and configure manifest

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,24 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      manifest: {
+        name: 'Speedrun Timer App',
+        short_name: 'STA',
+        description: 'A highly customizable speedrun timer app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }),
   ],
 });
