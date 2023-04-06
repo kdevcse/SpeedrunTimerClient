a7c756fa8875dfe09826a8c15802af2ae9a720fd
Kev <kdevcse@protonmail.com>
2023-04-06 00:05:23 -0500

    Migrate to electron forge project

--- README.md
+++
@@ -1,2 +0,0 @@
-# SpeedrunTimerClient
- A highly customizable speedrun timer app. Created as a project for a dev blog series as seen [here.](https://dev.to/kevthedev/making-a-speedrun-timer-prologue-4di)
---
+++ src/renderer/helpers/timer-worker-helper.js
@@ -0,0 +1,37 @@
+// Class wrapper for stopwatch Web Worker
+export class TimerWorker {
+  constructor() {
+    const url = new URL('../workers/timer-worker', import.meta.url);
+    this.worker = new Worker(url, {
+      type: "module"
+    });
+  }
+
+  setOnMessageFunc(onMsgFunc) {
+    this.worker.onmessage = onMsgFunc;
+  }
+
+  postMessageToWorker(data) {
+    this.worker.postMessage(data);
+  }
+
+  terminate() {
+    this.worker.terminate();
+  }
+}
+
+export const WorkerCommands = {
+  START: 0,
+  STOP: 1,
+  RESET: 2
+}
+
+// Communicates with Web Worker from main thread
+export const WorkerCommunicator = {
+  setOnMessageFunc: (func) => {
+    onmessage = func;
+  },
+  postMessageToMainThread: (data) => {
+    postMessage(data);
+  }
+};
