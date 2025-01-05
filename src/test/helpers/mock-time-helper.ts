55c1cd2f14ca1625412d5f99d06c6a67118f2108
Kev <klewiscse@gmail.com>
2025-01-05 16:20:43 -0600

    Init new tauri stuff

--- src/test/helpers/mock-time-helper.ts
+++
@@ -1,7 +0,0 @@
-import { vi } from "vitest";
-
-export async function waitForTime(timeToWait: number) {
-  await vi.runOnlyPendingTimersAsync();
-  vi.setSystemTime(new Date(vi.getMockedSystemTime() as Date).getTime() + timeToWait);
-  await vi.advanceTimersToNextTimerAsync();
-}
